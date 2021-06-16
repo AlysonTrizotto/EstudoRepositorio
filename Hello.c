aadd2574f7bd9f1b5e2c77ac8bcc6967b71767f2
AlysonTrizotto <alysontrizotto1@gmail.com>
2021-06-16 13:01:19 -0300

    Estudando Repositorio git
    Teste de push

---
+++ Hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+	printf("Bom dia!");
+	printf("Estou aprendendo a programar em C");
+	printf("Estou lendo a %d unidade do livro", 1);
+	printf("%s e uma diciplida importande do curso","Esta");
+	printf("%f", 57.35);
+	return(0);
+}
